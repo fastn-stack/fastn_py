9d00e11f2bbacbef2e8a7e5a848cdb77a0f00263
siddhantCodes <siddhantk232@gmail.com>
2023-11-23 17:20:21 +0530

    make fastn.django a django app not whole package

--- fastn/django/apps.py
+++ fastn/django/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FastnAuthMiddlewareConfig(AppConfig):
+class FastnConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "fastn"
+    name = "fastn.django"
