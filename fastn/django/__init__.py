8f1e8af2ce14bb85f1f60624572ca8b4a73d1cb6
Amit Upadhyay <upadhyay@gmail.com>
2023-11-29 12:41:53 +0530

    django form stuff

--- fastn/django/__init__.py
+++ fastn/django/__init__.py
@@ -1,21 +1,51 @@
 import logging
 import json
 
+from django import forms
 from django.conf import settings
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
-from django.http import HttpRequest
+import django.http
 from django.utils.deprecation import MiddlewareMixin
 
 import fastn.utils as utils
 
+
 logger = logging.getLogger(__name__)
-
 SECRET_KEY = getattr(settings, "FASTN_SECRET_KEY", getattr(settings, "SECRET_KEY", ""))
-
 COOKIE_NAME = "github"
-
 CI = utils.AESCipher(SECRET_KEY)
+
+
+class Form(forms.Form):
+
+    def __init__(self, request):
+        self.request = request
+        data = json.loads(request.body.decode("utf-8"))
+        super(Form, self).__init__(data)
+
+    def fastn_error_response(self):
+        return django.http.JsonResponse(
+            {"errors": self.errors}
+        )
+
+
+class RequestType:
+
+    def __init__(self):
+        self.user: typing.Optional[django.contrib.auth.models.User] = None
+        self.GET = django.http.QueryDict(mutable=True)
+        self.POST = django.http.QueryDict(mutable=True)
+        self.COOKIES = {}
+        self.META = {}
+        self.FILES = django.utils.datastructures.MultiValueDict()
+
+        self.path = ""
+        self.path_info = ""
+        self.method = None
+        self.resolver_match = None
+        self.content_type = None
+        self.content_params = None
 
 
 class GithubAuthMiddleware(MiddlewareMixin):
@@ -61,7 +91,7 @@ class GithubAuthMiddleware(MiddlewareMixin):
 
         login(request, user)
 
-    def process_request(self, request: HttpRequest):
+    def process_request(self, request: RequestType):
         """
         Use process_request instead of defining __call__ directly;
         Django's middleware layer will process_request in a coroutine in __acall__ if it detects an async context.
@@ -71,12 +101,11 @@ class GithubAuthMiddleware(MiddlewareMixin):
         return self._add_user(request)
 
 
-
 class DisableCSRFOnDebug(MiddlewareMixin):
 
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def process_request(self, request):
+    def process_request(self, request: RequestType):
         if settings.DEBUG:
             setattr(request, '_dont_enforce_csrf_checks', True)
